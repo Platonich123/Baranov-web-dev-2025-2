b6aa3f75b34d06d5c546c5485d5ec789c54be550
Platonich123 <145435938+Platonich123@users.noreply.github.com>
2025-06-06 18:17:29 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from whitenoise import WhiteNoise
 from lab1.app import app as lab1_app
 from lab2.app import app as lab2_app
 from lab3.app import app as lab3_app
+from lab4.app import app as lab4_app
 from root_app.app import app as root_app
 
 app = Flask(__name__)
@@ -11,7 +12,8 @@ app = Flask(__name__)
 app.wsgi_app = DispatcherMiddleware(root_app, {
     '/lab1': lab1_app,
     '/lab2': lab2_app,
-    '/lab3': lab3_app
+    '/lab3': lab3_app,
+    '/lab4': lab4_app
 })
 
 # Добавляем поддержку статических файлов через Whitenoise
